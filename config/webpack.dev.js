5decee42ea0c02c89cc103139b8f8a5a8d57b100
styx11 <1654630248@qq.com>
2019-05-08 20:55:37 +0800

    :wrench: update webpack dev mode config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,6 +5,15 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    proxy: {
+      '/poetry': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/poetry': '/poetry'
+        }
+      }
+    }
   }
 });
