6930db367b38cf4d13118cc17dad6cb1c096d36a
styx11 <1654630248@qq.com>
2019-05-10 20:27:59 +0800

    :bento: update py test file

--- lib/py_test.py
+++ lib/py_test.py
@@ -4,11 +4,15 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Child_Process Test')
 parser.add_argument('--word', type=str, required=False)
+parser.add_argument('--num', type=str, required=False)
 
 args = parser.parse_args()
 word = args.word
+num = args.num
 
 if word :
   print(word)
+  if num :
+    print(num)
 else :
   print('Hello From Python 中文测试')
