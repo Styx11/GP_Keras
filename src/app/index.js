cd4c9249fc98dd6d4f390ce66a6e488e1d2e6285
styx11 <1654630248@qq.com>
2019-05-18 20:41:50 +0800

    :sparkles: add app entrance file

---
+++ src/app/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue/dist/vue.esm';
+import axios from 'axios';
+
+
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    
+  },
+  data () {
+    return {
+      
+    };
+  },
+  template: `
+    <div id='main'>
+      
+    </div>
+  `,
+  methods: {
+    
+  }
+});
