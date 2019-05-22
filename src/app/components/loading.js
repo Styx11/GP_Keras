e3fa89b2f0accdafcbb8472fad6744301a0b19e8
styx11 <1654630248@qq.com>
2019-05-22 20:08:52 +0800

    :sparkles: add component loading

---
+++ src/app/components/loading.js
@@ -0,0 +1,22 @@
+import Vue from 'vue/dist/vue.esm';
+
+export default Vue.extend({
+  template: `
+    <div class='loading'>
+      <div class="sk-circle">
+        <div class="sk-circle1 sk-child"></div>
+        <div class="sk-circle2 sk-child"></div>
+        <div class="sk-circle3 sk-child"></div>
+        <div class="sk-circle4 sk-child"></div>
+        <div class="sk-circle5 sk-child"></div>
+        <div class="sk-circle6 sk-child"></div>
+        <div class="sk-circle7 sk-child"></div>
+        <div class="sk-circle8 sk-child"></div>
+        <div class="sk-circle9 sk-child"></div>
+        <div class="sk-circle10 sk-child"></div>
+        <div class="sk-circle11 sk-child"></div>
+        <div class="sk-circle12 sk-child"></div>
+      </div>
+    </div>
+  `
+})
