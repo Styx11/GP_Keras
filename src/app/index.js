7a2e4ec7634b1cfc4ebef7583a85b8aa473106ff
styx11 <1654630248@qq.com>
2019-05-22 20:12:00 +0800

    :sparkles: add dependency of component loading

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue/dist/vue.esm';
 import axios from 'axios';
 
-
+import Loading from './components/loading';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    
+    Loading,
   },
   data () {
     return {
