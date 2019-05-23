ee2ae44767b1172fb22d7e2edfd2280f84b371c7
styx11 <1654630248@qq.com>
2019-05-23 20:02:06 +0800

    :sparkles: add component inputs

---
+++ src/app/components/inputs.js
@@ -0,0 +1,19 @@
+import Vue from 'vue/dist/vue.esm';
+import isChinese from 'is-chinese';
+
+export default Vue.extend({
+  name: 'Inputs',
+  data () {
+    return {
+      
+    }
+  },
+  template: `
+    <div class='input-blocks'>
+      
+    </div>
+  `,
+  methods: {
+    
+  }
+})
