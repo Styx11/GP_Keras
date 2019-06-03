6e9c8238a48d311d2a81d68e6d220d8ba0b712d6
styx11 <1654630248@qq.com>
2019-06-03 20:50:21 +0800

    :sparkles: add component share

---
+++ src/app/components/result/components/share.js
@@ -0,0 +1,26 @@
+import Vue from 'vue/dist/vue.esm';
+import 'Share/css/share.min.css';
+import 'Share/js/social-share.min.js';
+
+export default Vue.extend({
+  name: 'Share',
+  props: {
+    
+  },
+  data () {
+    return {
+      
+    }
+  },
+  template: `
+    <div class='share'>
+      
+    </div>
+  `,
+  mounted () {
+    
+  },
+  methods: {
+    
+  }
+});
