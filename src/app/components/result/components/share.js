9f09a12e0eb7eb4c3387d7f635c78271055a8911
styx11 <1654630248@qq.com>
2019-06-04 19:49:44 +0800

    :sparkles: update component share

--- src/app/components/result/components/share.js
+++ src/app/components/result/components/share.js
@@ -5,22 +5,49 @@ import 'Share/js/social-share.min.js';
 export default Vue.extend({
   name: 'Share',
   props: {
-    
+    dataUrl: {
+      type: String
+    },
   },
   data () {
     return {
-      
+      config: {
+        mode: 'prepend',
+        url: 'https://github.com/Styx11/GP_Keras',
+        image: 'https://s2.ax1x.com/2019/05/29/Vu7Ots.png',
+        initialized: true,
+        wechatQrcodeTitle: '请打开微信扫一扫',
+      }
     }
   },
   template: `
     <div class='share'>
-      
+      <div class='share-bg' @click='hideShare'></div>
+      <img class='share-img' :src='dataUrl' ref='img'/>
+      <div class='share-btns'>
+        <div class='social-share'>
+          <a href='#' class='social-share-icon icon-weibo'></a>
+          <a href='#' class='social-share-icon icon-qq'></a>
+          <a href='#' class='social-share-icon icon-wechat'></a>
+          <a href='#' class='social-share-icon icon-qzone'></a>
+          <a href='#' class='social-share-icon icon-douban'></a>
+          <a
+            class='social-share-icon iconfont share-download'
+            @click.prevent='downloadShare'
+          >&#xe6c0;</a>
+        </div>
+      </div>
     </div>
   `,
   mounted () {
-    
+    window.socialShare('.social-share', this.config);
   },
   methods: {
-    
+    hideShare () {
+      this.$emit('hideShare');
+    },
+    downloadShare () {
+      this.$emit('downloadShare');
+    }
   }
 });
