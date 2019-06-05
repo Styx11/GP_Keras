8c1885f80da1199945e5d4948dbb0362cbfbd98a
styx11 <1654630248@qq.com>
2019-06-05 21:25:45 +0800

    :sparkles: add component result

---
+++ src/app/components/result/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue/dist/vue.esm';
+import domToImg from 'dom-to-image';
+import { saveAs } from 'file-saver';
+
+import Share from './components/share';
+
+export default Vue.extend({
+  name: 'Result',
+  components: {
+    Share
+  },
+  props: {
+    
+  },
+  data () {
+    return {
+      
+    }
+  },
+  computed: {
+    
+  },
+  template: `
+    <div class='result-blocks'>
+      
+    </div>
+  `,
+  mounted () {
+    
+  },
+  methods: {
+    
+  }
+});
